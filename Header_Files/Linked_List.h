e333fd0fd2eeca3e54719c73e48bccb763403990
Mayank Jha <70832880+mayank7jha1@users.noreply.github.com>
2025-08-20 14:06:29 +0530

    Update Linked_List.h

--- Header_Files/Linked_List.h
+++ Header_Files/Linked_List.h
@@ -200,13 +200,14 @@ ListNode<T>* reverseBetween(ListNode<T>* head, T left, T right) {
 
 
 //We are now reversing the values using recursion.
-template<typename T>
+
 
 //This solve function is there for iteration only and that too for end pointer.
-ListNode<T>*start = nullptr;
-bool stop = 0;
 
-void Solve(ListNode<int>*end, T left, T right) {
+ListNode<int>*start = NULL;
+bool stop = 0;
+template<typename T>
+void Solve(ListNode<T>*end, int left, int right) {
 
 	if (right == 1) {
 		return;
@@ -233,11 +234,241 @@ void Solve(ListNode<int>*end, T left, T right) {
 	return;
 }
 
-
-ListNode<T>* reverseBetween(ListNode<T>* head, T left, T right) {
+template<typename T>
+ListNode<T>* reverseBetween(ListNode<T>* head, int left, int right) {
 	start = head;
 	Solve(head, left, right);
 	return head;
+}
+
+
+template<typename T>
+void Cycle_Creation(ListNode<T>*head) {
+
+	if (head == NULL or head->next == NULL) {
+		//This Linked List cannot have a cycle.
+		return;
+	}
+
+	//Reach to the tail.
+	ListNode<T>*tail = head;
+	while (tail->next != NULL) {
+		tail = tail->next;
+	}
+
+	tail->next = head->next->next;
+}
+
+template<typename T>
+bool IsCycle(ListNode<T>*head) {
+
+	ListNode<T>*slow = head;
+	ListNode<T>*fast = head;
+
+	while (fast != NULL and fast->next != NULL) {
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+template<typename T>
+void Break_Cycle(ListNode<T>*head) {
+
+	if (IsCycle(head) == 0) {
+		return;
+	}
+
+	ListNode<T>*slow = head;
+	ListNode<T>*fast = head;
+
+	while (fast != NULL and fast->next != NULL) {
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow) {
+			break;
+		}
+	}
+
+	slow = head;
+	ListNode<T>*prev = head;
+	while (prev->next != fast) {
+		prev = prev->next;
+	}
+
+	while (fast != slow) {
+		fast = fast->next;
+		slow = slow->next;
+		prev = prev->next;
+	}
+
+	// while (prev->next != fast) {
+	// 	prev = prev->next;
+	// }
+
+	prev->next = NULL;
+
+	//Printing the cycle:
+	while (fast != NULL) {
+		cout << fast->val << "-->";
+		fast = fast->next;
+	}
+
+	cout << "NULL" << endl;
+}
+
+
+template < typename T>
+bool isPalindrome(ListNode<T>* head) {
+
+	//For 0 length Linked List and 1 length Linked List.
+	if (head == NULL or head->next == NULL) {
+		return true;
+	}
+
+	ListNode<T>*M = MidPoint3(head);//Logic Based on First Mid Point of In Case of Even Linked List
+	ListNode<T>*Sh = M->next;
+	M->next = NULL;
+
+	ReverseLLIteratively(Sh);
+
+	while (Sh != NULL) {
+		if (head->val != Sh->val) {
+			return false;
+		}
+
+		head = head->next;
+		Sh = Sh->next;
+	}
+
+	return true;
+}
+
+
+template<typename T>
+void reorderList(ListNode<T>* head) {
+
+	ListNode<T>*h1 = head;
+	ListNode<T>*M = MidPoint2(head);//Logic Based on Second Mid Point of In Case of Even Linked List
+	ListNode<T>*h2 = M->next;
+	M->next = NULL;
+
+	ReverseLLIteratively(h2);
+
+	//Merging:
+	ListNode<T>*x, *y;
+	while (h2 != NULL) {
+
+		x = h1->next;
+		y = h2->next;
+
+		h1->next = h2;
+		h1 = x;
+		h2->next = h1;
+		h2 = y;
+	}
+
+}
+
+
+template<typename T>
+ListNode<T>* partition(ListNode<T>* head, int x) {
+
+	ListNode<T>*n = NULL, *bh = NULL, *bt = NULL, *ch = NULL, *ct = NULL;
+
+	//Task: To Create two new Linked list :
+	while (head != NULL) {
+
+		n = head->next;
+
+		if (head->val < x) {
+			//This node will be part of chota linked list.
+
+			if (ct == NULL) {
+				//This means my chota linked list is empty till now.
+				ch = ct = head;
+				//Break:
+				head->next = NULL;
+				head = n;
+
+			} else {
+				//This means my chota linked list is not empty.
+				ct->next = head;
+				ct = head;
+				head->next = NULL;
+				head = n;
+
+			}
+
+
+		} else {
+			//This node will be part of bigger linked list.
+
+			if (bt == NULL) {
+				//My bigger linked list is empty.
+				bh = bt = head;
+				//Break
+				head->next = NULL;
+				head = n;
+			} else {
+
+				bt->next = head;
+				bt = head;
+				head->next = NULL;
+				head = n;
+
+			}
+
+		}
+
+	}
+
+
+	if (ct != NULL) {
+		ct->next = bh;
+		return ch;
+
+	} else {
+		return bh;
+	}
+}
+
+template<typename T>
+ListNode<T>* oddEvenList(ListNode<T>* head) {
+
+	if (head == NULL or head->next == NULL) {
+		return head;
+	}
+
+	ListNode<T>*evenhead = head->next;
+	ListNode<T>*oddhead = head;
+
+	ListNode<T>*current = head, *n = NULL, *prev = NULL;
+	int jump = 1;//Ye batayega ki aapki linked list odd length ki thi ya even.
+
+	while (current != NULL and current->next != NULL) {
+		n = current->next;
+		current->next = n->next;
+		prev = current;
+		current = n;
+		jump++;
+
+	}
+
+	if (jump % 2 == 1) {
+		current->next = evenhead;
+	} else {
+		prev->next = evenhead;
+	}
+
+	return oddhead;
+
 }
 
 
