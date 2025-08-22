8b329065a84f745689d98e34c764c8ead1ddd42f
Mayank Jha <70832880+mayank7jha1@users.noreply.github.com>
2025-08-22 18:14:16 +0530

    Update ListNode.h

--- Header_Files/ListNode.h
+++ Header_Files/ListNode.h
@@ -6,14 +6,17 @@ class ListNode {
 public:
 	T val;
 	ListNode *next;//Self Referencing Pointers.
+	// ListNode *prev;
 
 	ListNode() {
 		next = NULL;
+		// prev = NULL;
 	}
 
 	ListNode(int x) {
 		val = x;
 		next = NULL;
+		// prev=NULL;
 	}
 
 };
